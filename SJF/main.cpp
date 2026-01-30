d497162d2b24f6ef6f5847abf87699e45182d1de
abhi-saurav-saroya <abhisauravsaroya06@gmail.com>
2026-01-30 22:55:32 +0530

    workflow corrected in main.cpp

---
+++ SJF/main.cpp
@@ -0,0 +1,13 @@
+#include "menu.h"
+
+#include<iostream>
+using namespace std;
+
+int main() {
+    cout << "----NON-PREEMPTIVE SHORTEST JOB FIRST (SJF) CPU SCHEDULING----" << endl << endl;
+
+    Dashboard dashboard;
+    dashboard.menu();
+
+    return 0;
+}
