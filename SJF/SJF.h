26731220961c5e1c94ea903a48fa01ac7cd153c1
abhi-saurav-saroya <abhisauravsaroya06@gmail.com>
2026-01-30 22:49:20 +0530

    Process struct and Processess class skeleton

---
+++ SJF/SJF.h
@@ -0,0 +1,36 @@
+#ifndef SJF_H
+#define SJF_H
+
+#include <string>
+#include <vector>
+
+struct Process {
+    std::string processID;
+    int arrivalTime;
+    int burstTime;
+
+    int completionTime = 0;
+    int waitingTime = 0;
+    int turnaroundTime = 0;
+
+    bool completed = false;
+};
+
+
+class Processes {
+    private:
+        std::vector<Process> processesList;
+    public:
+        // core
+        bool addProcess(Process process);
+        bool deleteProcess(const std::string& processID);
+        bool clearAllProcesses();
+        bool scheduleSJF();
+        float computeAverageWaitingTime();
+
+        // helpers
+        const std::vector<Process>& getProcesses() const;
+        bool processIDExists(const std::string& processID) const;
+};
+
+#endif
