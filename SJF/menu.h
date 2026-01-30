c908f7116cb1e32d4225a2d49275445e0f1f31a0
abhi-saurav-saroya <abhisauravsaroya06@gmail.com>
2026-01-30 22:51:39 +0530

    Dashboard functions declared in menu.h

---
+++ SJF/menu.h
@@ -0,0 +1,19 @@
+#ifndef MENU_H
+#define MENU_H
+
+#include "SJF.h"
+
+class Dashboard {
+    public:
+        void menu();
+    private:
+        Processes scheduler;
+        void addProcess();
+        void deleteProcess();
+        void listInputProcesses();
+        void clearAllProcesses();
+        void displayGanttChart();
+        void showAverageWaitingTime();
+};
+
+#endif
