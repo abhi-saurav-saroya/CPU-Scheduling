ea12453e29abebfdf6e2b83f860c1ca2f7fd0f4c
abhi-saurav-saroya <abhisauravsaroya06@gmail.com>
2026-01-31 14:10:46 +0530

    application flow in main.cpp

---
+++ FCFS/main.cpp
@@ -0,0 +1,12 @@
+#include "menu.h"
+
+#include<iostream>
+
+int main() {
+    std::cout << "----NON-PREEMPTIVE FIRST COME FIRST SERVE (FCFS) CPU SCHEDULING----" << std::endl << std::endl;
+
+    Dashboard dashboard;
+    dashboard.menu();
+
+    return 0;
+}
